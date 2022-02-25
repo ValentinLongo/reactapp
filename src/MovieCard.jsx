1bb3398041f198672c336f81b45225511d129d22
ValentinLongo <longovalentin@gmail.com>
2022-02-24 23:24:37 -0300

    Grid modules

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,7 +1,9 @@
+import styles from "./MovieCard.module.css";
+
 export function MovieCard({movie}) {
     const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
-    return <li>
-        <img src={imageUrl} alt={movie.title} />
+    return <li className={styles.movieCard}>
+        <img width={230} height={345} className={styles.movieImage} src={imageUrl} alt={movie.title} />
         {movie.title}
         </li>
 }
