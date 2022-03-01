85fa08e0e925fd95efa3003bfb642a05829b496c
ValentinLongo <longovalentin@gmail.com>
2022-03-01 19:40:41 -0300

    Movies details

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import './App.module.css';
 import { MoviesGrid } from './MoviesGrid';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { MovieDetails } from './MovieDetails';
+import { LandingPage } from './LandingPage';
 
 //solo header
 export function App() {
   return (
-    <div>
-      <header>
-        <h1>Peliculas</h1>
-      </header>
-      <main>
-        <MoviesGrid />
-      </main>
-    </div>
+    <Router>
+        <header>
+          <Link to="/"><h1>Peliculas</h1></Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path='/' element={<LandingPage/>} />
+            <Route path='/movies/:MoviesID' element={<MovieDetails/>} />
+          </Routes>
+        </main>
+    </Router>
   );
 }
 
